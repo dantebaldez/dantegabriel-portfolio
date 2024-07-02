64846dacec934e38ed80d9cf6bc8c03905ec2cca
Dante <dantegl2000@gmail.com>
2024-07-02 00:35:34 -0300

    Updated title bar and choose a favicon, but coudnt change the icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import MainContent from './Components/MainContent'
 import Sidebar from './Components/Sidebar'
+import { useEffect } from 'react';
 
 import './Styles/components/app.sass'
 
 function App() {
+  useEffect(() => {
+    document.title = "Dante Gabriel DEV";
+  }, []);
+
   return (
     <div id="portfolio">
-			<h1>Dante Gabriel</h1>
-			<Sidebar />
-			<MainContent />
+      <h1>Dante Gabriel</h1>
+      <Sidebar />
+      <MainContent />
     </div>
-  )
+  );
 }
 
-export default App
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import App from './App.jsx'
 
 import './Styles/main.sass'
 
+const link =document.createElement('link');
+link.rel = 'icon'
+link.href = '/favicon.ico'
+document.head.appendChild(link);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
