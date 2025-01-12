494e592c6c6f2e74bba2861c960b2de1198e2201
Dante <47676539+dantebaldez@users.noreply.github.com>
2025-01-12 18:32:54 -0300

    Update AboutContainer.jsx

--- src/Components/AboutContainer.jsx
+++ src/Components/AboutContainer.jsx
@@ -1,8 +1,14 @@
 const AboutContainer = () => {
 	return <section className="about-container">
 		<h2>Sobre</h2>
-		<p>Meu nome é Dante Gabriel, sou um Dev Front End especializado em React.JS porém fiquei apaixonado pela área q posso aprender outros frameworks com certa facilidade</p>
-		<p>Além de desenvolvedor, uma área que me atrai bastante é a área de gerência de projetos, na faculdade em quase todos os projetos em grupo eu tomava a liderança e o projeto ficava muito bom, a equipe toda colaborava entre si e o andamento ia bem. Estou atualmente indo atrás de uma certificação de Scrum Master pois é a metodologia que eu mais acho viável atualmente e  foi a que eu mais estudei e utilizei em projetos da faculdade</p>
+		<p>Olá! Meu nome é Dante Gabriel, sou desenvolvedor front-end com especialização em React.js. Minha paixão pela área me permite aprender novos frameworks e tecnologias com facilidade, sempre buscando maneiras de aprimorar minhas habilidades e entregar soluções criativas e eficientes.</p>
+
+<p>Além do desenvolvimento, tenho grande interesse em gestão de projetos. Durante minha graduação, assumi a liderança em diversos trabalhos em grupo, o que me proporcionou experiências valiosas em coordenação, comunicação e motivação de equipes. Esses projetos não apenas alcançaram excelentes resultados, mas também destacaram a importância da colaboração para o sucesso.</p>
+
+<p>Atualmente, estou em busca da certificação Scrum Master, pois considero essa metodologia ágil uma das mais eficazes e produtivas, sendo também a que mais utilizei nos projetos acadêmicos e pessoais. Estou sempre pronto para novos desafios, seja liderando equipes ou desenvolvendo interfaces incríveis.</p>
+
+<p>Se você busca alguém comprometido, flexível e apaixonado por tecnologia, ficarei feliz em conversar! Entre em contato comigo para discutirmos como posso contribuir para o seu próximo projeto ou equipe.</p>
+
 	</section>
 }
 
