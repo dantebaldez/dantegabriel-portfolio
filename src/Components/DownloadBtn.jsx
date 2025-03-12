6e2819141dd57c1848e35bed2de1208aa0da452a
Dante <47676539+dantebaldez@users.noreply.github.com>
2025-03-12 14:16:12 -0300

    Update DownloadBtn.jsx

--- src/Components/DownloadBtn.jsx
+++ src/Components/DownloadBtn.jsx
@@ -2,7 +2,7 @@ import '../Styles/components/downloadbtn.sass'
 
 const DownloadBtn = () => {
 	const handleDownload = () => {
-			const url = 'https://drive.google.com/file/d/1rtsr5ATGSOHnXhw0Y2SwNSw2qvtsISFE/view?usp=sharing' 
+			const url = 'https://drive.google.com/file/d/1SMyQR7hycFO5X6ELDtIf7egVZpJs--fV/view?usp=sharing' 
 			window.open(url, '_blank');
 		};
 	return(
